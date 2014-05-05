a5f2714a3e5935b4c2ccad2a8c2885770c3e1c9d
corchwll <Tobias.Fertig@t-online.de>
2014-05-05 10:55:33 +0200

    updated ChessBoard.java

--- ChessArr/src/ChessBoard.java
+++ ChessArr/src/ChessBoard.java
@@ -1,6 +1,47 @@
+import java.io.Reader;
+import java.io.Writer;
+
 //Copyright © 2014 Tobias Fertig, Daniel Knogl
 
 public class ChessBoard 
 {
-
+	char[][] board;
+	int moveNumber;
+	char onMove;
+	
+	public ChessBoard()
+	{
+		this.board = new char[5][6];
+		this.moveNumber = 1;
+		this.onMove = 'W';
+	}
+	
+	public ChessBoard(String init)
+	{
+		
+	}
+	
+	public ChessBoard(Reader reader)
+	{
+		
+	}
+	
+	public String toString()
+	{
+		String result = "";
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 6; j++)
+			{
+				result = result + board[i][j];
+			}
+		}
+		result = result + ":" + moveNumber + ":" + onMove;
+		return result;
+	}
+	
+	public void print(Writer writer)
+	{
+		
+	}
 }
